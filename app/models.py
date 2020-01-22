e82effe7e53a7a30eafe21e66eb491fe5ee2ef2d
Maxwel5 <maxwellwafulasim@gmail.com>
2020-01-22 12:34:19 +0300

    Add a class user

--- app/models.py
+++ app/models.py
@@ -2,3 +2,10 @@ from . import db
 from datetime import datetime 
 from flask_login import UserMixin
 from . import login_manager
+
+class User(UserMixin,db.MOdel):
+    __tablename__="users"
+    id =db.Column(db.Integer,Primary_key = True)
+    username = db.Column(db.Integer,Primary_key = True)
+    password = db.Column(db.Integer,Primary_key = True)
+    score = db.Column(db.Integer,Primary_key = True)
