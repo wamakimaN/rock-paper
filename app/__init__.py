916fe41b3432eb767b6c8fe558533d5302e6e1a9
Maxwel5 <maxwellwafulasim@gmail.com>
2020-01-22 13:10:24 +0300

    Add modify model user

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,11 @@ from flask import Flask
 from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
+login_manager = LoginManager()
+
 db = SQLAlchemy()
 
 def create_app(config_name):
@@ -15,6 +18,7 @@ def create_app(config_name):
 
     # Initializing flask extensions
     bootstrap.init_app(app)
+    login_manager.init_app(app)
     db.init_app(app)
 
     # Will add the views and forms
--- app/models.py
+++ app/models.py
@@ -3,9 +3,12 @@ from datetime import datetime
 from flask_login import UserMixin
 from . import login_manager
 
-class User(UserMixin,db.MOdel):
+class User(UserMixin,db.Model):
     __tablename__="users"
-    id =db.Column(db.Integer,Primary_key = True)
-    username = db.Column(db.Integer,Primary_key = True)
-    password = db.Column(db.Integer,Primary_key = True)
-    score = db.Column(db.Integer,Primary_key = True)
+    id =db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(60),unique = True,nullable = False)
+    password = db.Column(db.String(200),nullable = False)
+    score = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"User('{self.username}')"
