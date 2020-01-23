ea3082eea59cc515229ac0b7480df1554614b8a8
Suad shire <jido@abdulazizs-MacBook-Air.local>
2020-01-23 11:59:17 +0300

    fixing files

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moses/Documents/Python/Blogposts/virtual"
+set -gx VIRTUAL_ENV "/Users/jido/Desktop/rock-paper/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
